3a0514912339e5ec53589924e31a6895309000e2
Kyle <foscat4o1k@outlook.com>
2018-12-02 15:49:11 -0600

    cors problem

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,15 +1,20 @@
-var keys = require("../keys.js")
+// var keys = require("../keys.js");
+var currentTime = moment().format("HH:mm:ss");
 
 $(document).on("click", "button", function() {
     var activity = $(this).attr("data-actChoice");
 
-    var queryUrl = "http://www.khanacademy.org/api/v1/exercises" + activity + "&" + keys.consumer;
+    var queryUrl = "http://www.khanacademy.org/exercise/" + "logarithms_1" + "&api_key=2XX8Y9z6N8PrxnnH";
 
     $.ajax({
         url: queryUrl,
-        method: "GET"
+        method: "GET",
+        oauth_consumer_key: "2XX8Y9z6N8PrxnnH",
+        oauth_timestamp: currentTime,
+        
     })
-      .then(function(response){
+      .then(function(err, response){
+          if (err) throw err;
           var result = response.data;
           console.log(result);
 
